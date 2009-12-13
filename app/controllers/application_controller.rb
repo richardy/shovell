1db0782261d579e413979a2b8f835475e835ac32
unknown <Administrator@.(none)>
2009-12-13 21:17:35 +1100

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  before_filter :fetch_logged_in_user
+
+  protected
+
+  def fetch_logged_in_user
+    return unless session[:user_id]
+    @current_user = User.find_by_id(session[:user_id])
+  end
 end
--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -8,6 +8,25 @@ class StoryTest < ActiveSupport::TestCase
 
   def test_should_create_story
     s = Story.create(:name => 'test name', :link => 'testlink.com')
-    assert s.valid?    
+    assert s.valid?
+  end
+
+  def test_should_not_be_valid_without_link
+    s = Story.create(:name => 'My test submission', :link => nil)
+    assert s.errors.on(:link)
+    end
+
+  def test_should_have_a_votes_association
+    assert_equal [ votes(:one), votes(:two) ], stories(:one).votes.find(:all, :order => 'id DESC')
+  end
+
+  def test_should_return_highest_vote_id_first
+    puts "one id = #{votes(:one).id}, two id = #{votes(:two).id}"
+    assert_equal votes(:two), stories(:one).votes.latest.first
+  end
+
+  def test_should_return_3_latest_votes
+    10.times { stories(:one).votes.create }
+    assert_equal 3, stories(:one).votes.latest.size
   end
 end
