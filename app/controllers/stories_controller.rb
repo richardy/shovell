b1d5545cb17c24591517fa68c0fc30ea3f4c117d
unknown <Administrator@.(none)>
2009-12-13 21:48:23 +1100

    .

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,8 @@
 class StoriesController < ApplicationController
+  before_filter :login_required, :only => [ :new, :create ]
+  
   def index
-    @story = Story.find_by_name('name')
+    @story = Story.find(:first, :order => 'RANDOM()')
   end
 
   def new
@@ -8,9 +10,9 @@ class StoriesController < ApplicationController
   end
 
   def create
-    @story = Story.new(params[:story])
+    @story = @current_user.stories.build params[:story]
     if @story.save
-      flash[:notice] = 'Story submission succeeded.'
+      flash[:notice] = 'Story submission succeeded'
       redirect_to stories_path
     else
       render :action => 'new'
